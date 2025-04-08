1711afc3efec5517b14d157e11964e6ef2a5deb7
Vikram Mullick <vikram1.mullick@gmail.com>
2025-04-07 23:07:22 -0400

    add macros for writing into derived memory blocks

--- virtualboy/vip.cpp
+++ virtualboy/vip.cpp
@@ -20,21 +20,24 @@ world_attributes_t::world_attributes_t()
     : memory_block_t(WORLD_ATTRIBUTES_SIZE) {}
 
 constexpr size_t FRAME_BUFFER_LEFT_0_START = 0x00000000;
-constexpr size_t FRAME_BUFFER_LEFT_0_END = 0x00005FFF;
-
 constexpr size_t WORLD_ATTRIBUTES_START = 0x0003D800;
-constexpr size_t WORLD_ATTRIBUTES_END = 0x0003DBFF;
+
+#define ADD_WRITE_H(buf, base)                                                 \
+    if (addr >= base##_START && addr < (base##_START + base##_SIZE)) {         \
+        buf.write_h(addr - base##_START, val);                                 \
+        return;                                                                \
+    }
+
+#define ADD_WRITE_H_SUFFIX(buf, base, suffix)                                  \
+    if (addr >= base##_##suffix##_START &&                                     \
+        addr < (base##_##suffix##_START + base##_SIZE)) {                      \
+        buf.write_h(addr - base##_##suffix##_START, val);                      \
+        return;                                                                \
+    }
 
 void vip_t::write_h(uint32_t addr, uint16_t val) {
-    if (addr >= FRAME_BUFFER_LEFT_0_START && addr <= FRAME_BUFFER_LEFT_0_END) {
-        m_frame_buffer_left_0.write_h(addr - FRAME_BUFFER_LEFT_0_START, val);
-        return;
-    }
-
-    if (addr >= WORLD_ATTRIBUTES_START && addr <= WORLD_ATTRIBUTES_END) {
-        m_world_attributes.write_h(addr - WORLD_ATTRIBUTES_START, val);
-        return;
-    }
+    ADD_WRITE_H_SUFFIX(m_frame_buffer_left_0, FRAME_BUFFER, LEFT_0);
+    ADD_WRITE_H(m_world_attributes, WORLD_ATTRIBUTES);
 
     cout << std::hex << addr << endl;
     assert(false);
