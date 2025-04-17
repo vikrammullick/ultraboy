e6fa55a695aebb3af7cf2bbf2b6ba459a16afc71
Vikram Mullick <vikram1.mullick@gmail.com>
2025-04-16 23:54:10 -0400

    refactor timer control register handling

--- virtualboy/timer.cpp
+++ virtualboy/timer.cpp
@@ -5,26 +5,38 @@
 
 using namespace std;
 
-#define ADD_MISC_REGISTER_OP(regaddr, reg, op)                                 \
-    if (addr == misc_hardware_registers::regaddr##_ADDR) {                     \
-        m_##reg.op(val);                                                       \
-        return;                                                                \
-    }
-
 void timer_t::write_misc_b(uint32_t addr, uint8_t val) {
-    ADD_MISC_REGISTER_OP(TCR, TCR, write);
+    if (addr == misc_hardware_registers::TCR_ADDR) {
+        m_T_Clk_Sel = val & (1 << 4);
+        m_Tim_Z_Int = val & (1 << 3);
+
+        bool try_clear_Z_Stat = val & (1 << 2);
+        bool new_T_Enb = val & (1 << 0);
+
+        // From Sacred Tech Scroll: If both T-Enb is cleared and Z-Stat-Clr is
+        // set while the timer is enabled, then the timer is disabled, but zero
+        // status is not cleared (nor is an interrupt acknowledged).
+        if (try_clear_Z_Stat && m_T_Enb && !new_T_Enb) {
+            m_Z_Stat = false;
+            // TODO: acknowledge timer zero interrupt: Z-Stat-Clr will clear
+            // Z-Stat and acknowledge a timer zero interrupt if the counter is
+            // non-zero or, if the counter is zero, if the timer is disabled.
+            // This bit is always set when read.
+        }
+
+        m_T_Enb = new_T_Enb;
+        return;
+    }
 
     cout << std::hex << addr << ": " << static_cast<uint16_t>(val) << endl;
     assert(false);
 }
 
-#define ADD_MISC_REGISTER_READ(reg)                                            \
-    if (addr == misc_hardware_registers::reg##_ADDR) {                         \
-        return m_##reg.read();                                                 \
-    }
-
 uint8_t timer_t::read_misc_b(uint32_t addr) {
-    ADD_MISC_REGISTER_READ(TCR);
+    if (addr == misc_hardware_registers::TCR_ADDR) {
+        return (m_T_Clk_Sel << 4) | (m_Tim_Z_Int << 3) | (true << 2) |
+               (m_Z_Stat << 1) | (m_T_Enb << 0);
+    }
 
     cout << std::hex << addr << endl;
     assert(false);
--- virtualboy/timer.hpp
+++ virtualboy/timer.hpp
@@ -3,42 +3,13 @@
 
 #include <cstdint>
 
-struct timer_control_register_t {
+class timer_t {
     bool m_T_Clk_Sel = false;
     bool m_Tim_Z_Int = false;
     bool m_Z_Stat = false;
     bool m_T_Enb = false;
 
-    uint8_t read() {
-        return (m_T_Clk_Sel << 4) | (m_Tim_Z_Int << 3) | (true << 2) |
-               (m_Z_Stat << 1) | (m_T_Enb << 0);
-    }
-
-    void write(uint8_t val) {
-        m_T_Clk_Sel = val & (1 << 4);
-        m_Tim_Z_Int = val & (1 << 3);
-
-        bool try_clear_Z_Stat = val & (1 << 2);
-        bool new_T_Enb = val & (1 << 0);
-
-        // From Sacred Tech Scroll: If both T-Enb is cleared and Z-Stat-Clr is
-        // set while the timer is enabled, then the timer is disabled, but zero
-        // status is not cleared (nor is an interrupt acknowledged).
-        if (try_clear_Z_Stat && m_T_Enb && !new_T_Enb) {
-            m_Z_Stat = false;
-            // TODO: acknowledge timer zero interrupt: Z-Stat-Clr will clear
-            // Z-Stat and acknowledge a timer zero interrupt if the counter is
-            // non-zero or, if the counter is zero, if the timer is disabled.
-            // This bit is always set when read.
-        }
-
-        m_T_Enb = new_T_Enb;
-    }
-};
-
-struct timer_t {
-    timer_control_register_t m_TCR;
-
+  public:
     void write_misc_b(uint32_t addr, uint8_t val);
     uint8_t read_misc_b(uint32_t addr);
 };
